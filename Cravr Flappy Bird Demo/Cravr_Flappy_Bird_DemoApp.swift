2658333a34a321bdcd455fc32fe50d5ff3da39bd
Ezra Akresh <ezraakresh@gmail.com>
2025-10-16 12:22:34 -0400

    Initial Commit

---
+++ Cravr Flappy Bird Demo/Cravr_Flappy_Bird_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cravr_Flappy_Bird_DemoApp.swift
+//  Cravr Flappy Bird Demo
+//
+//  Created by Ezra Akresh on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Cravr_Flappy_Bird_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
