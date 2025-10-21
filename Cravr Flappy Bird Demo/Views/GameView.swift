020b33c0720d5efd7f866d41ca734e50ff0ce17a
Ezra Akresh <ezraakresh@gmail.com>
2025-10-21 16:52:28 -0400

    created new bounding rectangles

--- Cravr Flappy Bird Demo/Views/GameView.swift
+++ Cravr Flappy Bird Demo/Views/GameView.swift
@@ -59,7 +59,7 @@ struct PipeView: View {
                 Rectangle()
                     .fill(Color.red.opacity(0.3))
                     .frame(width: GameConstants.pipeWidth, height: pipe.topHeight + 300) // Consistent width, variable height
-                    .clipShape(TriangleCutRectangle(cutCorners: [.bottomRight], triangleSize: 40))
+                    .clipShape(TriangleCutRectangle(cutCorners: [.bottomLeft, .bottomRight], triangleSize: 40, triangleSize2: 20))
                     .position(x: pipe.x - 19.5, y: pipe.topHeight / 2 - 150) // Match collision detection positioning
             }
             
@@ -75,7 +75,7 @@ struct PipeView: View {
                 Rectangle()
                     .fill(Color.red.opacity(0.3))
                     .frame(width: GameConstants.pipeWidth, height: pipe.bottomHeight + 300) // Consistent width, variable height
-                    .clipShape(TriangleCutRectangle(cutCorners: [.topRight], triangleSize: 40))
+                    .clipShape(TriangleCutRectangle(cutCorners: [.topRight, .topLeft], triangleSize: 40, triangleSize2: 20))
                     .position(x: pipe.x - 19.5, y: UIScreen.main.bounds.height - (pipe.bottomHeight / 2) + 150) // Match collision detection positioning
             }
         }
@@ -85,7 +85,7 @@ struct PipeView: View {
 struct TriangleCutRectangle: Shape {
     let cutCorners: [UIRectCorner]
     let triangleSize: CGFloat
-    
+    let triangleSize2: CGFloat
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
@@ -94,14 +94,14 @@ struct TriangleCutRectangle: Shape {
         
         // Cut out triangles from specified corners
         for corner in cutCorners {
-            let trianglePath = createTrianglePath(in: rect, corner: corner, size: triangleSize)
+            let trianglePath = createTrianglePath(in: rect, corner: corner, size: triangleSize, size2: triangleSize2)
             path = path.subtracting(trianglePath)
         }
         
         return path
     }
     
-    private func createTrianglePath(in rect: CGRect, corner: UIRectCorner, size: CGFloat) -> Path {
+    private func createTrianglePath(in rect: CGRect, corner: UIRectCorner, size: CGFloat, size2: CGFloat) -> Path {
         var trianglePath = Path()
         
         switch corner {
@@ -121,6 +121,26 @@ struct TriangleCutRectangle: Shape {
             let trianglePoint1 = CGPoint(x: rect.maxX - size, y: rect.maxY)
             let trianglePoint2 = CGPoint(x: rect.maxX, y: rect.maxY - size)
             trianglePath.move(to: bottomRight)
+            trianglePath.addLine(to: trianglePoint1)
+            trianglePath.addLine(to: trianglePoint2)
+            trianglePath.closeSubpath()
+            
+        case .topLeft:
+            // Triangle pointing down and right from top-left corner
+            let topLeft = CGPoint(x: rect.minX, y: rect.minY)
+            let trianglePoint1 = CGPoint(x: rect.minX + size2, y: rect.minY)
+            let trianglePoint2 = CGPoint(x: rect.minX, y: rect.minY + size2)
+            trianglePath.move(to: topLeft)
+            trianglePath.addLine(to: trianglePoint1)
+            trianglePath.addLine(to: trianglePoint2)
+            trianglePath.closeSubpath()
+            
+        case .bottomLeft:
+            // Triangle pointing up and right from bottom-left corner
+            let bottomLeft = CGPoint(x: rect.minX, y: rect.maxY)
+            let trianglePoint1 = CGPoint(x: rect.minX + size2, y: rect.maxY)
+            let trianglePoint2 = CGPoint(x: rect.minX, y: rect.maxY - size2)
+            trianglePath.move(to: bottomLeft)
             trianglePath.addLine(to: trianglePoint1)
             trianglePath.addLine(to: trianglePoint2)
             trianglePath.closeSubpath()
