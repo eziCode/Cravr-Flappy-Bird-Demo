34566886fa7210f381b2ec28915a9e572865735b
Ezra Akresh <ezraakresh@gmail.com>
2025-10-28 12:30:27 -0400

    screen scrolling in game now

--- Cravr Flappy Bird Demo/ScrollingBackground.swift
+++ Cravr Flappy Bird Demo/ScrollingBackground.swift
@@ -11,19 +11,12 @@ import Combine
 struct ScrollingBackgroundImage: View {
     @State private var offset: CGFloat = 0
     
-    let timer = Timer.publish(every: 0.016, on: .main, in: .common).autoconnect() // ~60fps
-    
     var body: some View {
         GeometryReader { geometry in
             let screenWidth = geometry.size.width
             let screenHeight = geometry.size.height
-            // Match pipe speed: basePipeSpeed = screenWidth * 0.015 per frame
-            // At 60fps: screenWidth * 0.015 / 0.016 = screenWidth * 0.9375 per second
             
-            VStack(spacing: 0) {
-                // Spacer() // Push content to bottom
-                
-                // We'll use two copies of the image side-by-side for seamless looping
+            TimelineView(.animation) { timeline in
                 HStack(spacing: 0) {
                     Image("flappy_sloth_background")
                         .resizable()
@@ -36,9 +29,9 @@ struct ScrollingBackgroundImage: View {
                         .frame(width: screenWidth)
                 }
                 .offset(x: offset)
-                .onReceive(timer) { _ in
+                .onChange(of: timeline.date) { _ in
                     // Scroll left at a smooth pace
-                    let scrollSpeed = screenWidth * 0.005 // Match pipe speed
+                    let scrollSpeed = screenWidth * 0.005
                     offset -= scrollSpeed
                     
                     // Reset offset when the first image is completely off-screen
